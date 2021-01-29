e82ad11a3d2f3ee4226741d236ff352ecb50e1f6
wghou <wghou09@bjtu.edu.cn>
2021-01-29 09:28:58 +0800

    init

---
+++ docs/index.rst
@@ -0,0 +1,10 @@
+pyvegafem Documentation
+============================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   pyvegafem
+
---
+++ tests/test.py
@@ -0,0 +1,5 @@
+import pyvegafem as vega
+
+assert vega.__version__ == '0.0.1'
+assert vega.add(1, 2) == 3
+assert vega.subtract(1, 2) == -1
